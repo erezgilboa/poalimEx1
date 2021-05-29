a83010bf1192443a75aa4170e40b80039cf90b15
e20775 <erezgi1974@gmail.com>
2021-05-29 11:41:24 +0300

    ex1

---
+++ src/main/java/com/poalim/microusers/filter/AndCriteria.java
@@ -0,0 +1,23 @@
+package com.poalim.microusers.filter;
+
+import com.poalim.microusers.entities.Person;
+import com.poalim.microusers.viewmodel.PersonVm;
+
+import java.util.List;
+
+public class AndCriteria  implements Criteria{
+
+    private Criteria criteria;
+    private Criteria otherCriteria;
+
+    public AndCriteria(Criteria criteria, Criteria otherCriteria) {
+        this.criteria = criteria;
+        this.otherCriteria = otherCriteria;
+    }
+
+    @Override
+    public List<PersonVm> meetCriteria(List<PersonVm> persons) {
+        List<PersonVm> firstCriteriaPersons = criteria.meetCriteria(persons);
+        return otherCriteria.meetCriteria(firstCriteriaPersons);
+    }
+}
